d986606f3ec9384d19ec26150288824622ea462c
chaychen2005 <605966652@qq.com>
2020-07-01 10:55:16 +0800

    modify table structure

--- src/main/java/org/fisco/bcos/safekeeper/data/DataController.java
+++ src/main/java/org/fisco/bcos/safekeeper/data/DataController.java
@@ -313,7 +313,7 @@ public class DataController extends BaseController {
         Instant startTime = Instant.now();
         // current
         String currentAccount = getCurrentAccount(request);
-        log.info("start wedpr/vcl/getCredentialList. startTime: {} account: {} value: {}",
+        log.info("start wedpr/vcl/v1/credentials/approve. startTime: {} account: {} value: {}",
                 startTime.toEpochMilli(), currentAccount, value);
 
         if (value <= 0) {
@@ -324,7 +324,7 @@ public class DataController extends BaseController {
         List<JsonNode> listOfData = dataService.preAuthorization(currentAccount, value);
         baseResponse.setData(listOfData);
 
-        log.info("end wedpr/vcl/getCredentialList. useTime:{} result:{}",
+        log.info("end wedpr/vcl/v1/credentials/approve. useTime:{} result:{}",
                 Duration.between(startTime, Instant.now()).toMillis(), JacksonUtils.objToString(baseResponse));
         return baseResponse;
     }
